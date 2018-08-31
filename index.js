a70ff9aef80701dc4ae35caee5651c835d2939f9
Workstation <matpapatol@gmail.com>
2018-08-31 13:03:41 +0300

    Added 2 new files to repository index.js && package.json
     On branch master
     Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	new file:   .gitignore
    	modified:   index.js
    	modified:   package.json

--- index.js
+++ index.js
@@ -1,34 +1,42 @@
 "use strict";
- 
+
 const AWS = require('aws-sdk');
+const request = require('request-promise');
+const promise = require('bluebird');
+
+const concurrency = 0;
+
 
 exports.handler = (event, context, callback) => {
 
-    const minimata=[];
-    
+    const minimata = [];
+
     console.log(JSON.stringify(event, null, 2));
-    event.Records.forEach((record) => {
-        console.log(record.eventID);
-        console.log(record.eventName);
-        console.log(record.dynamodb.NewImage.Message_body);
-        console.log('DynamoDB Record: %j', record.dynamodb);
-    });
+
+    for (let i = 0; i < event.Records.length; i++) {
+
+        const myMessage_body = event.Records[i].dynamodb.NewImage.Message_body;
+        console.log(myMessage_body);
+        const myMessage_id = event.Records[i].dynamodb.NewImage.Message_id;
+        console.log(myMessage_id);
+        console.log(event.Records[i].eventName);
+        minimata[i] = [myMessage_body, myMessage_id];
+        console.log('DynamoDB Record: %j', event.Records[i].dynamodb);
+    }
+
+    console.log("ta minimata einai: " + JSON.stringify(minimata, null, 2));
 
     const dedomena = event.Records;
-    console.log("ayta einai ta dedomena " + JSON.stringify(dedomena,null,2));
-
+    console.log("ayta einai ta dedomena ");
 
     const megethos = dedomena.length;
-    console.log("to megethos tou records einai : "+megethos);
+    console.log("to megethos tou records einai : " + megethos);
 
-    // for(let i =0; i< dedomena.length; i++ ){
-    //     minimata[i] =[event.Records[i].dynamodb.NewImage[0],event.Records[i].dynamodb.NewImage[1]];
-    // }
-    //console.log("Ta minimata einai :"+JSON.stringify(minimata,null,2));
+    /*    
+       return Promise.map(dedomena, function()
+    
+    
 
-
-
-//    Promise.map(dedomena, function()
-//)
-
+    //)
+    */
 };
